be734daf4c695c36ab526f66d8647c7a8d6a9cd0
csdiehl <csdiehl16@gmail.com>
2021-12-07 22:01:40 -0800

    set up database

--- init_db.py
+++ init_db.py
@@ -6,7 +6,9 @@ print("opened database connection")
 conn.execute('CREATE TABLE users (username TEXT UNIQUE, user_id INTEGER PRIMARY KEY) ')
 print("user table added")
 
-conn.execute('CREATE TABLE indoor (user_id INTEGER NOT NULL, date TEXT, grade TEXT, type TEXT, FOREIGN KEY (user_id) REFERENCES users (user_id))')
+conn.execute('CREATE TABLE indoor (user_id INTEGER NOT NULL, date TEXT, grade TEXT, type TEXT, num_routes INTEGER, send_type TEXT, FOREIGN KEY (user_id) REFERENCES users (user_id))')
 print("log table added")
+
+conn.execute('')
 
 conn.close()
